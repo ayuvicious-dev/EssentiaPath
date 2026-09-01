3ca3a7191597cd171d20e486045ae5475f2e9b43
ayuvicious-dev <ayuvicious@gmail.com>
2026-09-01 14:27:22 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // EssentiaPath service worker
 // Bump CACHE_NAME any time index.html (or other cached assets) changes,
 // so returning users get the fresh version instead of a stale cache.
-const CACHE_NAME = "essentiapath-cache-v105";
+const CACHE_NAME = "essentiapath-cache-v106";
 const ASSETS = [
   "./",
   "./index.html",
